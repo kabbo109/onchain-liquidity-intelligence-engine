25edbe663f24d8910d5b47f4f7da123eaa701771
Kabbo | Frontend Developer <160781484+kabbo109@users.noreply.github.com>
2026-01-18 13:59:45 +0600

    Create token.registry.js

---
+++ token.registry.js
@@ -0,0 +1,14 @@
+export const TOKENS = {
+  TOKEN_A: {
+    symbol: "TKA",
+    decimals: 18
+  },
+  TOKEN_B: {
+    symbol: "TKB",
+    decimals: 18
+  },
+  TOKEN_C: {
+    symbol: "TKC",
+    decimals: 18
+  }
+};
